05dfff6e8a3f9364c82101613fd9aeece22cfde5
Abdoul <abdoul.m2408@gmail.com>
2020-01-01 14:37:56 +0100

    bdd changes

--- streaming_world.sql
+++ streaming_world.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2.1
--- http://www.phpmyadmin.net
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
 --
--- Client :  localhost
--- Généré le :  Mar 31 Décembre 2019 à 14:16
--- Version du serveur :  5.7.28-0ubuntu0.16.04.2
--- Version de PHP :  7.0.33-0ubuntu0.16.04.7
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  mer. 01 jan. 2020 à 13:37
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -26,16 +28,18 @@ SET time_zone = "+00:00";
 -- Structure de la table `artworks`
 --
 
-CREATE TABLE `artworks` (
-  `Id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `artworks`;
+CREATE TABLE IF NOT EXISTS `artworks` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(120) NOT NULL,
   `Url` varchar(500) NOT NULL,
   `Image` varchar(120) NOT NULL,
-  `Image_Cover` varchar(150) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `Image_Cover` varchar(150) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
--- Contenu de la table `artworks`
+-- Déchargement des données de la table `artworks`
 --
 
 INSERT INTO `artworks` (`Id`, `Name`, `Url`, `Image`, `Image_Cover`) VALUES
@@ -53,7 +57,8 @@ INSERT INTO `artworks` (`Id`, `Name`, `Url`, `Image`, `Image_Cover`) VALUES
 -- Structure de la table `orderline`
 --
 
-CREATE TABLE `orderline` (
+DROP TABLE IF EXISTS `orderline`;
+CREATE TABLE IF NOT EXISTS `orderline` (
   `Order_Id` int(11) NOT NULL,
   `Product_Id` int(11) NOT NULL,
   `Quantity_Ordered` int(200) NOT NULL,
@@ -66,14 +71,16 @@ CREATE TABLE `orderline` (
 -- Structure de la table `orders`
 --
 
-CREATE TABLE `orders` (
-  `Id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE IF NOT EXISTS `orders` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
   `User_Id` int(11) NOT NULL,
   `TaxRate` double DEFAULT NULL,
   `TaxAmount` double DEFAULT NULL,
   `TotalAmount` double DEFAULT NULL,
   `CreationTimestamp` datetime DEFAULT NULL,
-  `CompleteTimestamp` datetime DEFAULT NULL
+  `CompleteTimestamp` datetime DEFAULT NULL,
+  PRIMARY KEY (`Id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -82,12 +89,14 @@ CREATE TABLE `orders` (
 -- Structure de la table `productline`
 --
 
-CREATE TABLE `productline` (
-  `ProductLine` varchar(50) COLLATE latin1_bin NOT NULL
+DROP TABLE IF EXISTS `productline`;
+CREATE TABLE IF NOT EXISTS `productline` (
+  `ProductLine` varchar(50) COLLATE latin1_bin NOT NULL,
+  PRIMARY KEY (`ProductLine`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_bin;
 
 --
--- Contenu de la table `productline`
+-- Déchargement des données de la table `productline`
 --
 
 INSERT INTO `productline` (`ProductLine`) VALUES
@@ -102,8 +111,9 @@ INSERT INTO `productline` (`ProductLine`) VALUES
 -- Structure de la table `products`
 --
 
-CREATE TABLE `products` (
-  `Id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE IF NOT EXISTS `products` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Artworks_Id` int(11) NOT NULL,
   `Name` varchar(120) NOT NULL,
   `Photo` varchar(90) NOT NULL,
@@ -111,15 +121,16 @@ CREATE TABLE `products` (
   `Description` varchar(900) NOT NULL,
   `QuantityInStock` int(255) NOT NULL,
   `BuyPrice` double NOT NULL,
-  `Price` double NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `Price` double NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
 
 --
--- Contenu de la table `products`
+-- Déchargement des données de la table `products`
 --
 
 INSERT INTO `products` (`Id`, `Artworks_Id`, `Name`, `Photo`, `ProductLine`, `Description`, `QuantityInStock`, `BuyPrice`, `Price`) VALUES
-(1, 2, 'SonGoku Ultra Instinct', 'GokuMui.png', 'figurines', 'Banpresto Figurine - DBZ - Son Goku FES Vol 8 - Ultra Instinct Son Goku - 20 cm ', 50, 0.8, 70),
+(1, 2, 'SonGoku Ultra Instinct Dragon Ball Super', 'GokuMui.png', 'figurines', 'Banpresto Figurine - DBZ - Son Goku FES Vol 8 - Ultra Instinct Son Goku - 20 cm ', 50, 0.8, 70),
 (2, 3, 'Naruto Shippuden Bijuu Mode', 'NarutoBijuu.jpg', 'figurines', 'TAMASHII NATIONS Naruto Shippuden Naruto Uzumaki -Kurama- Kizuna Relation, BandaiFiguartsZERO', 35, 0.8, 42),
 (3, 4, 'Monkey D. Luffy 4ème', 'LuffyGear4.jpg', 'figurines', 'One Piece Modèle Surdimensionné Monkey D. Luffy 4ème Scène Statue Décoration D\'animation', 120, 0.8, 128),
 (5, 1, 'Kamado Tanjiro', 'tanjiro.jpg', 'figurines', 'Demon Slayer PVC Action Figures Kamado Tanjirou Tenth Style The Dragon of Change Anime Kimetsu no Yaiba Figurine', 500, 15, 50),
@@ -130,7 +141,8 @@ INSERT INTO `products` (`Id`, `Artworks_Id`, `Name`, `Photo`, `ProductLine`, `De
 (24, 1, 'Tanjiro Pikachu version', 'PokeTanjiro.jpg', 'figurines', 'Véritable anime phénomène,  Kimetsu no Yaiba a déboulé dans la vie de nombreux fans de manga et d\'anime sans prévenir. Mais puisque créer des figurines à l\'effigie des personnages de l\'anime était trop simple, la société Game Harbors a souhaité mettre à l\'honneur un crossover un peu particulier : Kimetsu no Yaiba x Pokémon. Tanjiro sous la forme d\'un Pikachu (13cm).', 500, 5, 15),
 (25, 1, 'Nezuko Pikachu version', 'PokeNezuko.jpg', 'figurines', 'Véritable anime phénomène,  Kimetsu no Yaiba a déboulé dans la vie de nombreux fans de manga et d\'anime sans prévenir. Mais puisque créer des figurines à l\'effigie des personnages de l\'anime était trop simple, la société Game Harbors a souhaité mettre à l\'honneur un crossover un peu particulier : Kimetsu no Yaiba x Pokémon. Nezuko sous la forme d\'un Pikachu (13cm).', 500, 5, 15),
 (26, 1, 'Tanjiro et Nezuko Pikachu version', 'PokeTanjiro&Nezuko.jpg', 'figurines', 'Véritable anime phénomène,  Kimetsu no Yaiba a déboulé dans la vie de nombreux fans de manga et d\'anime sans prévenir. Mais puisque créer des figurines à l\'effigie des personnages de l\'anime était trop simple, la société Game Harbors a souhaité mettre à l\'honneur un crossover un peu particulier : Kimetsu no Yaiba x Pokémon. Ainsi, Tanjiro et Nezuko s\'illustrent alors sous la forme d\'un Pikachu (13cm).', 500, 10, 22.5),
-(27, 2, 'GOGETA SSJ BLUE 52CM', 'gogita.png', 'figurines', 'Figurine de Gogeta en Super Saiyan Blue\r\nConçus et peint à la mains, la reproduction est fidèle au personnage de la série Dragon Ball.\r\nLa figurine contient des led.\r\nMatière: Résine haute qualité\r\nDimension: 52cm\r\nÉdition: Édition limitée\r\nÉditeur: Figure Class\r\nPièces: 240pcs', 500, 100, 175);
+(27, 2, 'GOGETA SSJ BLUE 52CM', 'gogita.png', 'figurines', 'Figurine de Gogeta en Super Saiyan Blue\r\nConçus et peint à la mains, la reproduction est fidèle au personnage de la série Dragon Ball.\r\nLa figurine contient des led.\r\nMatière: Résine haute qualité\r\nDimension: 52cm\r\nÉdition: Édition limitée\r\nÉditeur: Figure Class\r\nPièces: 240pcs', 500, 100, 175),
+(28, 2, 'Statue Dragon Ball Z Majin Végéta HQS Plus By Tsume 54cm', 'majin.jpeg', 'figurines', 'La statue représente le prince des Saiyans sur le point de lancer son attaque – parfois appelée “Atomic Blast” – contre Majin Buu.\r\nIl porte sur le front la marque du sceau de possession de Babidi dont il vient tout juste de se libérer.\r\nSa tenue porte les stigmates de son combat face à Goku, et des éclairs l\'entourent… signe signe de la puissance du Super Saiyan 2.', 500, 25, 55);
 
 -- --------------------------------------------------------
 
@@ -138,25 +150,28 @@ INSERT INTO `products` (`Id`, `Artworks_Id`, `Name`, `Photo`, `ProductLine`, `De
 -- Structure de la table `streaming`
 --
 
-CREATE TABLE `streaming` (
-  `Id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `streaming`;
+CREATE TABLE IF NOT EXISTS `streaming` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Artworks_Id` int(11) NOT NULL,
   `Caption` varchar(120) NOT NULL,
+  `Status` int(200) NOT NULL,
   `Description` varchar(900) NOT NULL,
   `Video` varchar(120) NOT NULL,
-  `CreationTimestamp` datetime NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `CreationTimestamp` datetime NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
--- Contenu de la table `streaming`
+-- Déchargement des données de la table `streaming`
 --
 
-INSERT INTO `streaming` (`Id`, `Artworks_Id`, `Caption`, `Description`, `Video`, `CreationTimestamp`) VALUES
-(1, 1, 'Kimetsu No Yaiba 01 VOSTFR', 'on verra', 'Kimetsu_no_Yaiba_01_VOSTFR.mp4', '2019-12-28 15:17:47'),
-(2, 1, 'Kimetsu No Yaiba 02 VOSTFR', 'plus tard', 'Kimetsu_no_Yaiba_02_VOSTFR.mp4', '2019-12-28 16:26:43'),
-(4, 7, 'Vinland Saga 24 VOSTFR', 't\'as vu le titre', 'Vinland_Saga_24_VOSTFR.mp4', '2019-12-31 10:28:00'),
-(5, 2, 'DBS Broly', 't\'as vu le titre', 'DBS_FILM_01_Broly_2018_VOSTFR.mp4', '2019-12-31 10:29:13'),
-(6, 1, 'Kimetsu No Yaiba 03 VOSTFR', 'tkt', 'Kimetsu_no_Yaiba_03_VOSTFR.mp4', '2019-12-31 10:37:46');
+INSERT INTO `streaming` (`Id`, `Artworks_Id`, `Caption`, `Status`, `Description`, `Video`, `CreationTimestamp`) VALUES
+(1, 1, 'Kimetsu No Yaiba 01 VOSTFR', 1, 'on verra', 'Kimetsu_no_Yaiba_01_VOSTFR.mp4', '2019-12-28 15:17:47'),
+(2, 1, 'Kimetsu No Yaiba 02 VOSTFR', 2, 'plus tard', 'Kimetsu_no_Yaiba_02_VOSTFR.mp4', '2019-12-28 16:26:43'),
+(4, 7, 'Vinland Saga 24 VOSTFR', 24, 't\'as vu le titre', 'Vinland_Saga_24_VOSTFR.mp4', '2019-12-31 10:28:00'),
+(5, 2, 'DBS Broly', 1, 't\'as vu le titre', 'DBS_FILM_01_Broly_2018_VOSTFR.mp4', '2019-12-31 10:29:13'),
+(6, 1, 'Kimetsu No Yaiba 03 VOSTFR', 3, 'tkt', 'Kimetsu_no_Yaiba_03_VOSTFR.mp4', '2019-12-31 10:37:46');
 
 -- --------------------------------------------------------
 
@@ -164,8 +179,9 @@ INSERT INTO `streaming` (`Id`, `Artworks_Id`, `Caption`, `Description`, `Video`,
 -- Structure de la table `users`
 --
 
-CREATE TABLE `users` (
-  `Id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
   `FirstName` varchar(120) NOT NULL,
   `LastName` varchar(120) NOT NULL,
   `Email` varchar(120) NOT NULL,
@@ -175,86 +191,19 @@ CREATE TABLE `users` (
   `City` varchar(120) NOT NULL,
   `Zip` varchar(11) NOT NULL,
   `Role` varchar(11) NOT NULL,
-  `CreationTimestamp` datetime NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `CreationTimestamp` datetime NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
--- Contenu de la table `users`
+-- Déchargement des données de la table `users`
 --
 
 INSERT INTO `users` (`Id`, `FirstName`, `LastName`, `Email`, `Pseudo`, `Password`, `Address`, `City`, `Zip`, `Role`, `CreationTimestamp`) VALUES
 (1, 'Abmane', 'Oussoul', 'admin@gmail.com', 'admin', '$2y$11$1cf67a5dea60152ac284fu.dmjXduH4H4JEW7C3vmtO/PI1mx9cAa', '05 avenue nord', 'Paris', '75010', 'admin', '2019-12-26 16:53:31'),
 (3, 'Kuzumo', 'Power', 'sallukhan0805@gmail.com', 'Omoshiroy', '$2y$11$423b35d4a0d61dbe6c816u.Ja2kSZFDmMAB7EhU53jPBvC0pLB75y', 'chez konoha ', 'Le village de beerus', '68125', 'user', '2019-12-27 17:34:38');
+COMMIT;
 
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `artworks`
---
-ALTER TABLE `artworks`
-  ADD PRIMARY KEY (`Id`);
-
---
--- Index pour la table `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`Id`);
-
---
--- Index pour la table `productline`
---
-ALTER TABLE `productline`
-  ADD PRIMARY KEY (`ProductLine`);
-
---
--- Index pour la table `products`
---
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`Id`);
-
---
--- Index pour la table `streaming`
---
-ALTER TABLE `streaming`
-  ADD PRIMARY KEY (`Id`);
-
---
--- Index pour la table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`Id`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `artworks`
---
-ALTER TABLE `artworks`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
---
--- AUTO_INCREMENT pour la table `orders`
---
-ALTER TABLE `orders`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `products`
---
-ALTER TABLE `products`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
---
--- AUTO_INCREMENT pour la table `streaming`
---
-ALTER TABLE `streaming`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
---
--- AUTO_INCREMENT pour la table `users`
---
-ALTER TABLE `users`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
